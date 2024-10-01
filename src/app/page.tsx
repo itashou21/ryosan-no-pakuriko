05a5a621ce993bd6816b84bc7943e75b5640e88b
itashou21 <itashou21@gmail.com>
2024-10-02 00:16:23 +0900

    最初のコミット

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import OfficePakuriko from "@/components/OfficePakuriko";
+
+export default function Home() {
+  return (
+    <main className="min-h-screen flex items-center justify-center bg-gray-100">
+      <OfficePakuriko />
+    </main>
+  );
+}
